e5ef6d5f4e668b57892ccbbbc564c442e4f9dc1a
Prasad Ravan <prasadcravan@gmail.com>
2022-08-01 23:53:46 +0530

    Day 1

---
+++ LeetCodeAlgorithmDay1Program1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeAlgorithmDay1Program1
+{
+    internal class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 4, 5, 6 };
+
+            Console.WriteLine(Search(nums, 9));
+            Console.ReadLine();
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            int minIndex = 0;
+            int maxIndex = nums.Length - 1;
+
+            while (minIndex <= maxIndex)
+            {
+                int midIndex = (minIndex + maxIndex) / 2;
+
+                if (target == nums[midIndex])
+                {
+                    return midIndex;
+                }
+                else if (target < nums[midIndex])
+                {
+                    maxIndex = midIndex - 1;
+                }
+                else
+                {
+                    minIndex = midIndex + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay1Program2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeAlgorithmDay1Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 4, 6 };
+            Console.WriteLine(SearchInsert(nums, 6));
+            Console.ReadLine();
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int minIndex = 0;
+            int maxIndex = nums.Length - 1;
+
+            while (minIndex <= maxIndex)
+            { 
+                int midIndex = minIndex + (maxIndex - minIndex) /2;
+
+                if (nums[midIndex] == target)
+                {
+                    return midIndex;
+                }
+                else if (nums[midIndex] > target)
+                {
+                    maxIndex = midIndex - 1;
+                }
+                else
+                {
+                    minIndex = midIndex + 1;
+                }
+            }
+            
+            return minIndex;
+        }
+    }
+}
