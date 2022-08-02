94783fec1bad1e72b0b04708839c2e754aba04d3
Prasad Ravan <prasadcravan@gmail.com>
2022-08-03 00:42:26 +0530

    Day2

---
+++ LeetCodeAlgorithmDay2Program1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeAlgorithmDay2Program1
+{
+    internal class SquaresOfSorted
+    {
+        static void Main(string[] args)
+        {
+            var sortedArray = SortedSquares(new int[] { -4, -2, 0, 5 });
+
+            foreach (int i in sortedArray)
+            { 
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] SortedSquares(int[] nums)
+        {
+            var squared = new int[nums.Length];
+
+            var beginIndex = 0;
+            var endIndex = nums.Length - 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (Math.Abs(nums[beginIndex]) > Math.Abs(nums[endIndex]))
+                {
+                    squared[i] = nums[beginIndex] * nums[beginIndex];
+                    beginIndex++;
+                }
+                else
+                {
+                    squared[i] = nums[endIndex] * nums[endIndex];
+                    endIndex--;
+                }
+            }  
+
+            return squared;
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay2Program2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeAlgorithmDay2Program2
+{
+    internal class RotateArray
+    {
+        static void Main(string[] args)
+        {
+            Rotate(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+            Console.ReadLine();
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+            var returnArray = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                returnArray[(i + k) % nums.Length] = nums[i];
+            }
+            
+            foreach (int i in returnArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
