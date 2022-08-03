e9911a6c897bc0449adb4a6165a5ff4c5bd0afd1
Prasad Ravan <prasadcravan@gmail.com>
2022-08-03 22:47:57 +0530

    Day3

---
+++ LeetCodeAlgorithmDay3Program1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeAlgorithmDay3Program1
+{
+    internal class MoveZero
+    {
+        static void Main(string[] args)
+        {
+            MoveZeroes(new int[] { 1, 0, 3, 0, 12 });
+            Console.ReadLine();
+        }
+
+        public static void MoveZeroes(int[] nums)
+        {
+            var zeroIndex = 0;
+            var nonZeroIndex = 0;
+
+            while (nonZeroIndex < nums.Length && zeroIndex < nums.Length)
+            {
+                if (nums[zeroIndex] == 0)
+                {
+                    if (nonZeroIndex > zeroIndex)
+                    {
+                        nums[zeroIndex] = nums[nonZeroIndex];
+                        nums[nonZeroIndex] = 0;
+                        nonZeroIndex++;
+                    }
+                    else
+                    {
+                        nonZeroIndex++;
+                    }
+                }
+                else
+                {
+                    zeroIndex++;
+                }
+            }
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay3Program2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeAlgorithmDay3Program2
+{
+    internal class TargetTwoSum
+    {
+        static void Main(string[] args)
+        {
+            var output = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+            Console.WriteLine(output[0]);
+            Console.WriteLine(output[1]);
+
+            Console.ReadLine();
+        }
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            var start = 0;
+            var end = numbers.Length - 1;
+
+            while (start < end)
+            {
+                if (numbers[start] + numbers[end] == target)
+                {
+                    return new int[2] { ++start, ++end };
+                }
+                else if (numbers[start] + numbers[end] < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+
+            return new int[2] { -1, -1 };
+        }
+    }
+}
