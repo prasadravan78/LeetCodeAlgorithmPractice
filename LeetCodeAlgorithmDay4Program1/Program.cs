9b3dbfebb7bca296da17af7008853768e7e416cd
Prasad Ravan <prasadcravan@gmail.com>
2022-08-04 21:57:53 +0530

    Day4

---
+++ LeetCodeAlgorithmDay4Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeAlgorithmDay4Program1
+{
+    internal class ReverseStringProgram
+    {
+        static void Main(string[] args)
+        {
+            ReverseString("Hello".ToCharArray());
+            Console.ReadLine();
+        }
+
+        public static void ReverseString(char[] s)
+        {
+            var head = 0;
+            var tail = s.Length -1;
+
+            while (head < tail)
+            {
+                (s[tail], s[head]) = (s[head], s[tail]);
+
+                head++;
+                tail--;
+            }
+
+            Console.WriteLine(new String(s));
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay4Program2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeAlgorithmDay4Program2
+{
+    internal class ReverseWordsInString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("Hello World"));
+            Console.ReadLine();
+        }
+
+        public static string ReverseWords(string s)
+        {
+            var head = 0;
+            var tail = s.Length -1;
+            var charArray = s.ToCharArray();
+
+            while (head < tail)
+            {
+                (charArray[tail], charArray[head]) = (charArray[head], charArray[tail]);
+
+                head++;
+                tail--;
+            }
+
+            s = new string(charArray);
+
+            var words = s.Split(' ');
+
+            head = 0;
+            tail = words.Length - 1;
+
+            while (head < tail)
+            {
+                (words[tail], words[head]) = (words[head], words[tail]);
+
+                head++;
+                tail--;
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
