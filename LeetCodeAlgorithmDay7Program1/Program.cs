ef3dde1775b0c84dc58a9ac22fd8d3bcdb85c9f3
Prasad Ravan <prasadcravan@gmail.com>
2022-08-07 23:14:44 +0530

    Day7

---
+++ LeetCodeAlgorithmDay7Program1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeAlgorithmDay7Program1
+{
+    internal class FloodFillDFS
+    {
+        static void Main(string[] args)
+        {
+            int[][] image = new int[2][];
+            image[0] = new int[3] { 0, 0, 0 };
+            image[1] = new int[3] { 0, 1, 0 };
+
+            Console.WriteLine(FloodFill(image, 0, 0, 2));
+            Console.ReadLine();
+        }
+
+        public static int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            var queue = new Queue<int[]>();
+            var existingColor = image[sr][sc];
+
+            if (existingColor == color)
+            {
+                return image;
+            }
+
+            image[sr][sc] = color;
+            queue.Enqueue(new int[] { sr, sc });
+
+            while (queue.Count() > 0)
+            {
+                var dequewed = queue.Dequeue();
+
+                if (dequewed[0] - 1 > -1 && image[dequewed[0] - 1][dequewed[1]] == existingColor)
+                {
+                    image[dequewed[0] - 1][dequewed[1]] = color;
+                    queue.Enqueue(new int[] { dequewed[0] - 1, dequewed[1] });
+                }
+                if (dequewed[1] - 1 > -1 && image[dequewed[0]][dequewed[1] - 1] == existingColor)
+                {
+                    image[dequewed[0]][dequewed[1] - 1] = color;
+                    queue.Enqueue(new int[] { dequewed[0], dequewed[1] - 1 });
+                }
+
+                if (dequewed[0] + 1 < image.Length && image[dequewed[0] + 1][dequewed[1]] == existingColor)
+                {
+                    image[dequewed[0] + 1][dequewed[1]] = color;
+                    queue.Enqueue(new int[] { dequewed[0] + 1, dequewed[1] });
+                }
+
+                if (dequewed[1] + 1 < image[0].Length && image[dequewed[0]][dequewed[1] + 1] == existingColor)
+                {
+                    image[dequewed[0]][dequewed[1] + 1] = color;
+                    queue.Enqueue(new int[] { dequewed[0], dequewed[1] + 1 });
+                }
+            }
+
+            return image;
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay7Program2/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCodeAlgorithmDay7Program2
+{
+    internal class IsLandArea
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new int[2][];
+            grid[0] = new int[3] { 0, 0, 0 };
+            grid[1] = new int[3] { 1, 1, 0 };
+
+            Console.WriteLine(MaxAreaOfIsland(grid));
+        }
+
+        public static int MaxAreaOfIsland(int[][] grid)
+        {
+            var maxArea = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        grid[i][j] = 0;
+                        maxArea = Math.Max(maxArea, GetMaxArea(grid, i, j));
+
+                    }
+                }
+            }
+
+            return maxArea;
+        }
+
+        public static int GetMaxArea(int[][] grid, int row, int col)
+        {
+            var totalArea = 1;
+
+            if (row - 1 > 0 && grid[row - 1][col] > 0)
+            {
+                grid[row - 1][col] = 0;
+                totalArea += GetMaxArea(grid, row - 1, col);
+            }
+
+            if (row + 1 < grid.Length && grid[row + 1][col] > 0)
+            {
+                grid[row + 1][col] = 0;
+                totalArea += GetMaxArea(grid, row + 1, col);
+            }
+
+            if (col + 1 < grid[0].Length && grid[row][col + 1] > 0)
+            {
+                grid[row][col + 1] = 0;
+                totalArea += GetMaxArea(grid, row, col + 1);
+            }
+
+            if (col - 1 > 0 && grid[row][col - 1] > 0)
+            {
+                grid[row][col - 1] = 0;
+                totalArea += GetMaxArea(grid, row, col - 1);
+            }
+
+            return totalArea;
+        }
+    }
+}
