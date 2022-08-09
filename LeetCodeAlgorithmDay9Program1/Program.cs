c95455eba715a4bda55e186260ea131b4c90ec26
Prasad Ravan <prasadcravan@gmail.com>
2022-08-10 00:41:50 +0530

    Day9

---
+++ LeetCodeAlgorithmDay9Program1/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCodeAlgorithmDay9Program1
+{
+    internal class DistanceToZero
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new int[3][];
+
+            matrix[0] = new int[] { 1, 1, 1 };
+            matrix[1] = new int[] { 1, 0, 1 };
+            matrix[2] = new int[] { 1, 1, 1 };
+
+            var result = UpdateMatrix(matrix);
+            Console.ReadLine();
+        }
+
+        public static int[][] UpdateMatrix(int[][] mat)
+        {
+            var list = new List<(int, int)>();
+            var directions = new int[][] { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
+
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[0].Length; j++)
+                {
+                    if (mat[i][j] == 0)
+                    {
+                        list.Add((i, j));
+                    }
+                    else
+                    {
+                        mat[i][j] = -1;
+                    }
+                }
+            }
+
+            var step = 1;
+
+            while (list.Count > 0)
+            {
+                var newList = new List<(int, int)>();
+
+                foreach (var (i, j) in list)
+                {
+                    foreach (var dir in directions)
+                    {
+                        var newI = i + dir[0];
+                        var newJ = j + dir[1];
+
+                        if (newI >= 0 && newI < mat.Length && newJ >= 0 && newJ < mat[0].Length && mat[newI][newJ] == -1)
+                        {
+                            newList.Add((newI, newJ));
+                            mat[newI][newJ] = step;
+                        }
+                    }
+                }
+
+                list = newList;
+                step++;
+            }
+
+            return mat;
+        }
+    }
+}
---
+++ LeetCodeAlgorithmDay9Program2/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCodeAlgorithmDay9Program2
+{
+    internal class RottingOranges
+    {
+        static void Main(string[] args)
+        {
+            var grid = new int[3][];
+
+            grid[0] = new int[] { 2, 1, 1 };
+            grid[1] = new int[] { 1, 1, 0 };
+            grid[2] = new int[] { 0, 1, 1 };
+
+            Console.WriteLine(OrangesRotting(grid));
+            Console.ReadLine();
+        }
+
+        public static int OrangesRotting(int[][] grid)
+        {
+            int freshCount = 0;
+            var queue = new Queue<(int, int)>();
+            var directions = new int[][] { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        freshCount++;
+                    }
+                    else if (grid[i][j] == 2)
+                    {
+                        queue.Enqueue((i, j));
+                    }
+                }
+            }
+
+            if (freshCount == 0)
+            {
+                return 0;
+            }
+
+            var minutes = -1;
+
+            while (queue.Any())
+            {
+                var count = queue.Count;
+
+                while (count > 0)
+                {
+                    var (i, j) = queue.Dequeue();
+
+                    foreach (var dir in directions)
+                    {
+                        var (newI, newJ) = (i + dir[0], j + dir[1]);
+
+                        if (newI >= 0 && newJ >= 0 && newI < grid.Length && newJ < grid[0].Length && grid[newI][newJ] == 1)
+                        {
+                            queue.Enqueue((newI, newJ));
+                            grid[newI][newJ] = 2;
+                            freshCount--;
+                        }
+                    }
+
+                    count--;
+                }
+                
+
+                minutes++;
+            }
+
+            return freshCount > 0 ? -1 : minutes;
+        }
+    }
+}
